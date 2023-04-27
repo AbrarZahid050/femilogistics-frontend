9399839fbb547fe6d85473c42936c3c54d854d94
abrarzahid050 <abrar.zahid050@gmail.com>
2023-04-27 20:55:51 +0500

    50% of new load page done!

--- src/components/Styles/StyledBtns.js
+++ src/components/Styles/StyledBtns.js
@@ -1,5 +1,14 @@
-import { styled, Button, FormLabel, InputBase } from "@mui/material";
-import { grey, indigo, blue } from "@mui/material/colors";
+import {
+  styled,
+  Button,
+  FormLabel,
+  InputBase,
+  Radio,
+  FormControlLabel,
+  useRadioGroup,
+  Switch,
+} from "@mui/material";
+import { grey, indigo } from "@mui/material/colors";
 
 export const LoginBtn = styled(Button)(() => {
   return {
@@ -40,21 +49,145 @@ export const NavbarBtn = styled(Button)(() => {
 });
 
 export const StyledLabel = styled(FormLabel)(() => ({
-  "&.Mui-focused": { color: blue[700] },
+  "&.Mui-focused": { color: "#0062FF" },
   "&.Mui-error": { color: "#d32f2f" },
-  color: "black",
+  color: "#282842",
   fontWeight: "500",
-  fontSize: "13px",
+  fontSize: "16px",
 }));
 
 export const StyledInput = styled(InputBase)(() => ({
   border: `1px solid ${grey[200]}`,
   background: grey[100],
   borderRadius: 5,
-  padding: "0 8px",
-  fontSize: "13px",
+  padding: "5px 8px",
+  color: "#282842",
+  fontSize: "16px",
   "&.Mui-focused": {
-    border: `1px solid ${blue[700]}`,
+    border: `1px solid #0062FF`,
   },
   "&.Mui-error": { border: `1px solid #d32f2f` },
+}));
+
+//--------------------------------------Styled Radio-------------------------------------------------//
+const BpIcon = styled("span")(() => ({
+  borderRadius: "50%",
+  width: 16,
+  height: 16,
+  boxShadow:
+    "inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)",
+  backgroundColor: "white",
+  backgroundImage:
+    "linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))",
+  ".Mui-focusVisible &": {
+    outline: "2px auto rgba(19,124,189,.6)",
+    outlineOffset: 2,
+  },
+  "input:hover ~ &": {
+    backgroundColor: "#ebf1f5",
+  },
+  "input:disabled ~ &": {
+    boxShadow: "none",
+    background: "rgba(206,217,224,.5)",
+  },
+}));
+
+const BpCheckedIcon = styled(BpIcon)({
+  backgroundColor: "#0062FF",
+  backgroundImage:
+    "linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))",
+  "&:before": {
+    display: "block",
+    width: 16,
+    height: 16,
+    backgroundImage: "radial-gradient(#fff,#fff 28%,transparent 32%)",
+    content: '""',
+  },
+  "input:hover ~ &": {
+    backgroundColor: "blue",
+  },
+});
+
+export function BpRadio(props) {
+  return (
+    <Radio
+      disableRipple
+      color="default"
+      checkedIcon={<BpCheckedIcon />}
+      icon={<BpIcon />}
+      {...props}
+    />
+  );
+}
+
+//----------------------------------------Styled label------------------------------------------------//
+
+const StyledFormControlLabel = styled((props) => (
+  <FormControlLabel {...props} />
+))(({ checked }) => ({
+  border: `1px solid ${grey[300]}`,
+  "&": checked && {
+    border: "1px solid #0062FF",
+    background: "rgba(0, 98, 255, 0.05)",
+  },
+}));
+
+export function MyFormControlLabel(props) {
+  const radioGroup = useRadioGroup();
+  let checked = false;
+
+  if (radioGroup) {
+    checked = radioGroup.value === props.value;
+  }
+
+  return <StyledFormControlLabel checked={checked} {...props} />;
+}
+
+//--------------------------------------Styled switch-------------------------------------------------//
+export const StyledSwitch = styled((props) => (
+  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+  width: 40,
+  height: 22,
+  padding: 0,
+  "& .MuiSwitch-switchBase": {
+    padding: 0,
+    margin: 2,
+    transitionDuration: "300ms",
+    "&.Mui-checked": {
+      transform: "translateX(18px)",
+      color: "#fff",
+      "& + .MuiSwitch-track": {
+        backgroundColor: "#0062FF",
+        opacity: 1,
+        border: 0,
+      },
+      "&.Mui-disabled + .MuiSwitch-track": {
+        opacity: 0.5,
+      },
+    },
+    "&.Mui-focusVisible .MuiSwitch-thumb": {
+      color: "#33cf4d",
+      border: "6px solid #fff",
+    },
+    "&.Mui-disabled .MuiSwitch-thumb": {
+      color: grey[200],
+    },
+    "&.Mui-disabled + .MuiSwitch-track": {
+      opacity: 0.7,
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    boxSizing: "border-box",
+    width: 18,
+    height: 18,
+  },
+  "& .MuiSwitch-track": {
+    borderRadius: 30,
+    backgroundColor: grey[400],
+    opacity: 1,
+    transition: theme.transitions.create(["background-color"], {
+      duration: 500,
+    }),
+  },
 }));
--- src/views/carrier/Carrier.jsx
+++ src/views/carrier/Carrier.jsx
@@ -1,84 +1,288 @@
-import { Paper, Box, Grid, Stack, Button } from "@mui/material";
+import {
+  Paper,
+  Box,
+  Grid,
+  Stack,
+  Button,
+  Typography,
+  FormControl,
+  RadioGroup,
+} from "@mui/material";
 import { blue, indigo } from "@mui/material/colors";
-import { StyledInput, StyledLabel } from "../../components/Styles/StyledBtns";
+import {
+  StyledInput,
+  StyledLabel,
+  BpRadio,
+  MyFormControlLabel,
+  StyledSwitch,
+} from "../../components/Styles/StyledBtns";
 
 const Carrier = () => {
   return (
-    <Box sx={{ width: "100%", padding: 1 }}>
-      <Paper sx={{ width: "100%", p: 2 }} elevation={6}>
-        <Grid
-          container
-          rowSpacing={2}
-          alignItems="center"
-          width="100%"
-          justifyContent="space-between"
+    <Box sx={{ width: "100%", padding: 1, background: "#F9F9F9" }}>
+      <Stack spacing={2}>
+        <Paper
+          sx={{
+            p: 2,
+            background: "#FFFFFF",
+            boxShadow:
+              "0px 0px 1px rgba(12, 26, 75, 0.24), 0px 3px 8px -1px rgba(50, 50, 71, 0.05)",
+            borderRadius: "8px",
+          }}
+          elevation={6}
         >
-          <Grid item display="inline-flex" gap={2}>
-            <Stack spacing={1} direction="row" alignItems="center">
-              <StyledLabel>Customer</StyledLabel>
-              <StyledInput
-                value="Customer Name"
-                readOnly
-                sx={{ color: blue.A400, width: "130px" }}
-              />
-            </Stack>
+          <Grid
+            container
+            rowSpacing={2}
+            alignItems="center"
+            width="100%"
+            justifyContent="space-between"
+          >
+            <Grid item display="inline-flex" gap={2}>
+              <Stack spacing={1} direction="row" alignItems="center">
+                <StyledLabel>Customer</StyledLabel>
+                <StyledInput
+                  value="Customer Name"
+                  readOnly
+                  sx={{ color: blue.A400, width: "180px" }}
+                />
+              </Stack>
 
-            <Stack spacing={1} direction="row" alignItems="center">
-              <StyledLabel>Status</StyledLabel>
-              <StyledInput value="Available" readOnly sx={{ width: "100px" }} />
-            </Stack>
+              <Stack spacing={1} direction="row" alignItems="center">
+                <StyledLabel>Status</StyledLabel>
+                <StyledInput
+                  value="Available"
+                  readOnly
+                  sx={{ width: "150px" }}
+                />
+              </Stack>
 
-            <Stack spacing={1} direction="row" alignItems="center">
-              <StyledLabel>Assinged To</StyledLabel>
-              <StyledInput
-                value="Guest Account"
-                readOnly
-                sx={{ width: "120px" }}
-              />
-            </Stack>
+              <Stack spacing={1} direction="row" alignItems="center">
+                <StyledLabel>Assinged To</StyledLabel>
+                <StyledInput
+                  value="Guest Account"
+                  readOnly
+                  sx={{ width: "150px" }}
+                />
+              </Stack>
+            </Grid>
+
+            <Grid item>
+              <Button
+                variant="contained"
+                sx={{
+                  background: indigo[900],
+                  fontSize: "15px",
+                  height: "30px",
+                  borderRadius: "7px",
+                }}
+              >
+                AR
+              </Button>
+
+              <Button
+                variant="contained"
+                sx={{
+                  background: indigo[900],
+                  fontSize: "15px",
+                  height: "30px",
+                  borderRadius: "7px",
+                  marginLeft: 2,
+                }}
+              >
+                AP
+              </Button>
+              <Button
+                variant="contained"
+                sx={{
+                  background: "#0062FF",
+                  fontSize: "15px",
+                  height: "30px",
+                  borderRadius: "7px",
+                  marginLeft: 2,
+                  width: "200px",
+                }}
+              >
+                Submit
+              </Button>
+            </Grid>
           </Grid>
+        </Paper>
 
-          <Grid item>
-            <Button
-              variant="contained"
-              sx={{
-                background: indigo[900],
-                fontSize: "12px",
-                height: "30px",
-                borderRadius: "7px",
-              }}
-            >
-              AR
-            </Button>
+        <Paper sx={{ p: 2 }}>
+          {/* main grid */}
+          <Grid container columnSpacing={2}>
+            {/* column 1 */}
+            <Grid item xs={6}>
+              <Stack spacing={0.5}>
+                {/* heading */}
+                <Typography fontSize="18px" fontWeight="500">
+                  Feight
+                </Typography>
 
-            <Button
-              variant="contained"
-              sx={{
-                background: indigo[900],
-                fontSize: "12px",
-                height: "30px",
-                borderRadius: "7px",
-                marginLeft: 2,
-              }}
-            >
-              AP
-            </Button>
-            <Button
-              variant="contained"
-              sx={{
-                background: "#0062FF",
-                fontSize: "12px",
-                height: "30px",
-                borderRadius: "7px",
-                marginLeft: 2,
-                width: "200px",
-              }}
-            >
-              Submit
-            </Button>
+                {/* first input */}
+                <FormControl>
+                  <Grid container alignItems="center">
+                    <Grid item xs={3}>
+                      <StyledLabel>Commodity</StyledLabel>
+                    </Grid>
+                    <Grid item xs={9}>
+                      <StyledInput fullWidth value="Truck/Van/N/A" />
+                    </Grid>
+                  </Grid>
+                </FormControl>
+
+                {/* second input */}
+                <FormControl>
+                  <Grid container alignItems="center">
+                    <Grid item xs={3}>
+                      <StyledLabel>Case</StyledLabel>
+                    </Grid>
+                    <Grid item xs={9}>
+                      <StyledInput fullWidth value="Miami" />
+                    </Grid>
+                  </Grid>
+                </FormControl>
+
+                {/* third input */}
+                <FormControl>
+                  <Grid container alignItems="center">
+                    <Grid item xs={3}>
+                      <StyledLabel>Mileage</StyledLabel>
+                    </Grid>
+                    <Grid item xs={9}>
+                      <StyledInput fullWidth value="John Doe" />
+                    </Grid>
+                  </Grid>
+                </FormControl>
+                {/* fourth input */}
+                <FormControl>
+                  <Grid container alignItems="center">
+                    <Grid item xs={3}>
+                      <StyledLabel>Transportation</StyledLabel>
+                    </Grid>
+                    <Grid item xs={9}>
+                      <StyledInput fullWidth value="Purus id pheretra" />
+                    </Grid>
+                  </Grid>
+                </FormControl>
+                {/* fifth input */}
+                <FormControl>
+                  <Grid container alignItems="center">
+                    <Grid item xs={3}>
+                      <StyledLabel>Equip. Min</StyledLabel>
+                    </Grid>
+                    <Grid item xs={9}>
+                      <StyledInput fullWidth value="Ut sit adipiscing id" />
+                    </Grid>
+                  </Grid>
+                </FormControl>
+              </Stack>
+            </Grid>
+
+            {/* column-2 */}
+            <Grid item xs={6}>
+              <Stack spacing={0.5}>
+                {/* first input */}
+                <FormControl>
+                  <Grid container alignItems="center">
+                    <Grid item xs={3}>
+                      <StyledLabel>Load Value</StyledLabel>
+                    </Grid>
+                    <Grid item xs={9}>
+                      <StyledInput fullWidth value="Ut sit adipiscing id" />
+                    </Grid>
+                  </Grid>
+                </FormControl>
+
+                {/* second input */}
+                <FormControl>
+                  <Grid container alignItems="center">
+                    <Grid item xs={3}>
+                      <StyledLabel>Weight</StyledLabel>
+                    </Grid>
+                    <Grid item xs={9}>
+                      <StyledInput fullWidth value="Ut sit adipiscing id" />
+                    </Grid>
+                  </Grid>
+                </FormControl>
+
+                {/* third input */}
+                <FormControl>
+                  <Grid container alignItems="center">
+                    <Grid item xs={3}>
+                      <StyledLabel>Refree Temp</StyledLabel>
+                    </Grid>
+                    <Grid item xs={9}>
+                      <StyledInput fullWidth value="Ut sit adipiscing id" />
+                    </Grid>
+                  </Grid>
+                </FormControl>
+
+                {/* fourth input */}
+                <FormControl>
+                  <Grid container alignItems="center">
+                    <Grid item xs={3}>
+                      <StyledLabel>Temperature</StyledLabel>
+                    </Grid>
+                    <Grid item xs={9}>
+                      <RadioGroup
+                        row
+                        aria-labelledby="demo-row-radio-buttons-group-label"
+                        name="row-radio-buttons-group"
+                        sx={{ padding: "0 10px" }}
+                      >
+                        <MyFormControlLabel
+                          sx={{
+                            paddingRight: "20px",
+                            borderRadius: "8px",
+                          }}
+                          value="F"
+                          control={<BpRadio />}
+                          label="F"
+                        />
+                        <MyFormControlLabel
+                          sx={{
+                            borderRadius: "8px",
+                            paddingRight: "20px",
+                          }}
+                          value="C"
+                          control={<BpRadio />}
+                          label="C"
+                        />
+                      </RadioGroup>
+                    </Grid>
+                  </Grid>
+                </FormControl>
+
+                {/* fifth input */}
+                <FormControl>
+                  <Grid container alignItems="center" height="40px">
+                    <Grid item xs={3}>
+                      <StyledLabel>Partial Vol</StyledLabel>
+                    </Grid>
+                    <Grid item xs={9} alignItems="center">
+                      <StyledSwitch />
+                    </Grid>
+                  </Grid>
+                </FormControl>
+
+                {/* sixth input */}
+                <FormControl>
+                  <Grid container alignItems="center" height="40px">
+                    <Grid item xs={3}>
+                      <StyledLabel>Team Service</StyledLabel>
+                    </Grid>
+                    <Grid item xs={9} alignItems="center">
+                      <StyledSwitch />
+                    </Grid>
+                  </Grid>
+                </FormControl>
+              </Stack>
+            </Grid>
           </Grid>
-        </Grid>
-      </Paper>
+        </Paper>
+      </Stack>
     </Box>
   );
 };
